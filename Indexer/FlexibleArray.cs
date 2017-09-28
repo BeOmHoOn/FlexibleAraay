9e0094a3715044cd68ef3022fc3238900e1ba094
BeOmHoOn <r_emak_e@naver.com>
2017-09-28 11:17:28 +0900

    C~ommit

---
+++ Indexer/FlexibleArray.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Indexer
+{
+    public class FlexibleArray<T> : IEnumerable<T>
+    {
+        private bool _isInit = false;
+        private T[] member;
+
+        public int Length => member.Length;
+
+        public FlexibleArray(int size = 0)
+        {
+            Resize(size);
+        }
+
+        public void Resize(int size)
+        {
+            if (!_isInit)
+            {
+                member = new T[size];
+                _isInit = true;
+                return;
+            }
+            else
+            {
+                T[] clone = new T[Length];
+                Array.Copy(member, clone, Length);
+
+                member = new T[size];
+                for (int idx = 0; idx < clone.Length; idx++)
+                {
+                    member[idx] = clone[idx];
+                }
+            }
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                return member[index];
+            }
+            set
+            {
+                if (index >= member.Length)
+                    Resize(index--);
+
+                member[index] = value;
+            }
+        }
+
+        #region [ IEnumerable Helper ]
+        public IEnumerator<T> GetEnumerator()
+        {
+            return ((IEnumerable<T>)member).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable<T>)member).GetEnumerator();
+        }
+        #endregion
+    }
+}
---
+++ Indexer/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Indexer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = new FlexibleArray<int>();
+
+            arr[3] = 6;
+            arr[7] = 3;
+            arr[1] = 1;
+            
+            foreach (var item in arr)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
