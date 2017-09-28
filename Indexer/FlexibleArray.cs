4fb6211afb2af9c0a9770a3a06e879d5519c78b7
BeOmHoOn <r_emak_e@naver.com>
2017-09-28 17:52:33 +0900

    Resolve Performance Issue

--- Indexer/FlexibleArray.cs
+++ Indexer/FlexibleArray.cs
@@ -6,61 +6,89 @@ namespace Indexer
 {
     public class FlexibleArray<T> : IEnumerable<T>
     {
-        private bool _isInit = false;
-        private T[] member;
+        private const int _maxArraySize = Int32.MaxValue;
+        private const int _defaultArraySize = 8;
 
-        public int Length => member.Length;
+        private T[] _items;
+
+        public T[] Items
+        {
+            get
+            {
+                var returnArray = new T[_size];
+                Array.Copy(_items, returnArray, _size);
+
+                return returnArray;
+            }
+        }
+
+        private int _size;
+        public int Length => _size;
+        public int TrueLength
+        {
+            get => _items.Length;
+            private set
+            {
+                if (value > 0 && value != TrueLength)
+                {
+                    var replaceItem = new T[value];
+
+                    if (_size > 0)
+                        Array.Copy(_items, 0, replaceItem, 0, _size);
+
+                    _items = replaceItem;
+                }
+            }
+        }
 
         public FlexibleArray(int size = 0)
         {
-            Resize(size);
+            if (size < 0)
+                throw new ArgumentException();
+
+            _items = new T[size];
         }
 
-        public void Resize(int size)
+        private void Resize(int size)
         {
-            if (!_isInit)
-            {
-                member = new T[size];
-                _isInit = true;
+            if (TrueLength >= size)
                 return;
-            }
-            else
-            {
-                T[] clone = new T[Length];
-                Array.Copy(member, clone, Length);
 
-                member = new T[size];
-                for (int idx = 0; idx < clone.Length; idx++)
-                {
-                    member[idx] = clone[idx];
-                }
-            }
+            if (size > _maxArraySize)
+                size = _maxArraySize;
+
+            _items = new T[TrueLength == 0 ? _defaultArraySize : TrueLength * 2];
         }
 
         public T this[int index]
         {
             get
             {
-                return member[index];
+                if (index >= _size)
+                    throw new ArgumentException();
+
+                return _items[index];
             }
             set
             {
-                if (index >= member.Length)
-                    Resize(index--);
+                if (index >= _items.Length)
+                    Resize(index + 1);
 
-                member[index] = value;
+                _size = Math.Max(_size, index + 1);
+
+                _items[index] = value;
             }
         }
 
         #region [ IEnumerable Helper ]
         public IEnumerator<T> GetEnumerator()
         {
-            return ((IEnumerable<T>)member).GetEnumerator();
+            return ((IEnumerable<T>)Items).GetEnumerator();
         }
 
         IEnumerator IEnumerable.GetEnumerator()
         {
-            return ((IEnumerable<T>)member).GetEnumerator();
+            return ((IEnumerable<T>)Items).GetEnumerator();
         }
         #endregion
     }
--- Indexer/Program.cs
+++ Indexer/Program.cs
@@ -1,16 +1,19 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
 namespace Indexer
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var arr = new FlexibleArray<int>();
+            var arr = new FlexibleArray<string>();
 
-            arr[3] = 6;
-            arr[7] = 3;
-            arr[1] = 1;
-            
+            arr[3] = "바보";
+            arr[7] = "멍튱이";
+            arr[1] = "앙대";
+
             foreach (var item in arr)
             {
                 Console.WriteLine(item);
